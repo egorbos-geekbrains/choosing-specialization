dd221845ec526f06a46dd10b42128df5d4e07b19
Sergei Egorov <null@egorbos.codes>
2023-03-14 17:06:15 +1000

    Добавлено решение задачи

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// Вспомогательные функции
+string[]? GetValues()
+{
+    Console.Write($"\nВведите элементы разделяя пробелом: ");
+    var values = Console.ReadLine()?.Split(" ").ToArray();
+    return values;
+}
+
+//
+Console.Clear();
+
+var array = GetValues();
+var filtered = string.Empty;
+
+while (array is null || array.Length < 2)
+{
+    array = GetValues();
+}
+
+foreach (var item in array)
+{
+    if (item.Length < 4)
+    {
+        filtered += $"{item}\n";
+    }
+}
+
+var result = filtered.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+
+Console.WriteLine($"Результат: [{string.Join(", ", result)}]");
